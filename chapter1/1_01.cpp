3584586930082f78b5f264a9ee49057e2ef030cc
soledadvb <37829316+soledadvb@users.noreply.github.com>
2022-06-04 21:53:23 +0800

    2022/6/4

---
+++ chapter1/1_01.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+bool fibon_elem(int, int &);
+
+int main()
+{
+    int pos;
+    cout << "Enter the position of the element: ";
+    cin >> pos;
+    int elem;
+    if ( fibon_elem( pos, elem))
+        cout << "elem # " << pos << " is " << elem << endl;
+        else cout << "sorry " << pos << " could not calculate element # " << pos << endl;
+}
+
+bool fibon_elem(int pos, int & elem)
+{
+    // 检查位置值是否合法
+    if ( pos <= 0 || pos > 1024)
+    {
+        elem = 0;
+        return false;
+    }
+    elem = 1;
+    int n_2 = 1, n_1 = 1;
+    for (int ix = 3; ix <= pos ; ++ix)
+    {
+        elem = n_2 + n_1;
+        n_2 = n_1;
+        n_1 = elem;
+    }
+    return true;
+}
+
+
+bool
---
+++ chapter2/2_02.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+ofstream ofil("text_out1");
+void display(vector<int> vec)
+{
+    for (int ix = 0; ix < vec.size(); ++ix)
+        cout << vec[ix] << ' ';
+    cout << endl;
+}
+
+void swap(int val1, int val2)
+{
+    ofil << "swap ( " << val1
+         << ", " << val2 << " ) \n";
+    int temp = val1;
+    val1 = val2;
+    val2 = temp;
+
+    ofil << "after swap(): val1 " << val1
+         << " val2: " << val2 << "\n";
+}
+
+void swap3(int &val1, int &val2)
+{
+    int temp = val1;
+    val1 = val2;
+    val2 = temp;
+}
+
+void bubble_sort(vector<int> &vec)
+{
+    for (unsigned int ix = 0; ix < vec.size(); ++ix)
+        for (int jx = ix + 1; jx < vec.size(); ++jx)
+            if (vec[ix] > vec[jx])
+            {
+                ofil << " about to call swap! "
+                     << " ix: " << ix << " jx: " << jx << '\t'
+                     << " swapping: " << vec[ix]
+                     << " with " << vec[jx] << endl;
+                swap3(vec[ix], vec[jx]);
+            }
+}
+
+int main()
+{
+    int ia[8] = {8, 34, 3, 13, 1, 21, 5, 2};
+    vector<int> vec(ia, ia + 8);
+
+    cout << "vector before sort: ";
+    display(vec);
+
+    bubble_sort(vec);
+
+    cout << "vector after sort: ";
+    display(vec);
+}
